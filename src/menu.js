9617559e01cb43bdae174cbf75c62a30008c1d1d
BuildTools <unconfigured@null.spigotmc.org>
2020-12-08 20:57:40 -0500

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,33 +1,25 @@
 const { TextChannel } = require('discord.js')
-const { Message } = require('discord.js')
 
 module.exports = class Menu {
-    static defaultReactions = { back: '◀️', next: '▶️' }
+    static defaultReactions = { back: '◀', next: '▶' }
 
     constructor(opts = {}) {
-        const { channel = new TextChannel, message = new Message, userID, pages = [], page = 0, time = 600000, reactions = Menu.defaultReactions, customCatch = () => {} } = opts
+        const { channel = new TextChannel, userID, pages = [], page = 0, time = 600000, reactions = Menu.defaultReactions, customCatch = console.error } = opts
         this.channel = channel
-        this.message = message
         this.pages = pages
         this.time = time
         this.reactions = reactions
         this.page = page
         this.catch = customCatch
-        if (this.pages.length > 1 && !this.pages[page].footer.text.includes(' - Page')) {
-            this.pages[page].footer.text += ` - Page ${page+1}/${this.pages.length}`
-        }
-        message.channel.send(pages[page]).then(msg => {
+        channel.send(pages[page]).then(msg => {
             this.msg = msg
             this.addReactions()
             this.createCollector(userID)
-        })
+        }).catch(customCatch)
     }
     select(pg = 0) {
         this.page = pg
-        if (this.pages.length > 1 && !this.pages[pg].footer.text.includes(' - Page')) {
-            this.pages[pg].footer.text += ` - Page ${pg+1}/${this.pages.length}`
-        }
-        this.msg.edit(this.pages[pg])
+        this.msg.edit(this.pages[pg]).catch(this.catch)
     }
     createCollector(uid) {
         const collector = this.msg.createReactionCollector((r, u) => u.id == uid, { time: this.time })
@@ -45,13 +37,11 @@ module.exports = class Menu {
         })
     }
     async addReactions() {
-        if (this.pages.length > 1) {
-            try {
-                if (this.reactions.back) await this.msg.react(this.reactions.back)
-                if (this.reactions.next) await this.msg.react(this.reactions.next)
-            } catch (e) {
-                this.catch(e)
-            }
+        try {
+            if (this.reactions.back)  await this.msg.react(this.reactions.back)
+            if (this.reactions.next)  await this.msg.react(this.reactions.next)
+        } catch (e) {
+            this.catch(e)
         }
     }
 }
